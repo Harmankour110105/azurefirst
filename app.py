3e101b9ba196d8bd3f43dea0a8c09e203e450afb
YOUR NAME <YOUR EMAIL@SAMLECOM>
2025-06-16 12:04:04 +0530

    1

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ ALLOWED_IPS = ['117.210.169.244']  # or maybe hardcoded IPs
 
 @app.before_request
 def limit_remote_addr():
-    ip = request.headers.get('X-Forwarded-For', request.remote_addr)
-    print(f"Visitor IP: {ip}")  # debug line
-    if ip not in ALLOWED_IPS:
+    remote_ip = request.remote_addr
+    if not any(remote_ip.endswith(ip) for ip in ALLOWED_IPS):
         abort(403)
         
 @app.route('/')
